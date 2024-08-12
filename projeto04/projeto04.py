c57e6ab468885cd2b441345770e244e4fdd459bc
paivasbr <contatobrendapaiva@outlook.com>
2024-08-12 15:08:20 -0300

    Finalizando

--- projeto04/projeto04.py
+++ projeto04/projeto04.py
@@ -2,10 +2,14 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 
 webcam = cv2.VideoCapture(0)
-rastreador = HandDetector(detectionCon=0.8)
+rastreador = HandDetector(detectionCon=0.8, maxHands=2)
 
 while True:
     sucesso, imagem = webcam.read()
+    coordenadas, hands_image = rastreador.findHands(imagem)
+
+    print(coordenadas)
+
     cv2.imshow("python - projeto_IA", imagem)
 
     if cv2.waitKey(1) != -1:
